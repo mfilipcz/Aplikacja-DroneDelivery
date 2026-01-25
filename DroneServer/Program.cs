e61e66abc7493624913d51a18d284a757a430100
Michał Filipczyk <mfilipcz@MacBook-Pro-Micha.local>
2026-01-25 10:47:49 +0100

    Finalny fix: Port 5000, HTTP/2, Animacje, Cache mapy

--- DroneServer/Program.cs
+++ DroneServer/Program.cs
@@ -1,14 +1,22 @@
 using DroneServer.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core; // Konieczne dla konfiguracji portu
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Dodaj usługi gRPC
+// --- NAPRAWA: Wymuszamy Port 5000 i HTTP/2 ---
+// To jest ten fragment, którego brakowało i przez to miałeś błędy.
+// Dzięki temu omijamy "zombie" na porcie 5011 i pasujemy do klienta.
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
+});
+// ---------------------------------------------
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Skonfiguruj serwis
 app.MapGrpcService<DroneApiService>();
-app.MapGet("/", () => "Serwer Dronów działa! Użyj klienta gRPC.");
+app.MapGet("/", () => "Serwer Dronów działa (Pamięć RAM, Port 5000, HTTP/2)!");
 
 app.Run();
